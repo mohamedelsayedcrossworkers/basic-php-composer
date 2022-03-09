f8b76aeb645ba2096ab3dbc851fafba129b62176
Mohamed Elsayed <mmi@crossworkers.com>
2022-03-09 15:19:12 +0200

    test pipline

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+
 use Carbon\Carbon;
 
 
@@ -9,3 +10,5 @@ printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-
 printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
 
 printf("Right now in Paris is %s\n", Carbon::now('Europe/Paris'));
+
+echo 'abc for test';
